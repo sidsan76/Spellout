077dbdf4c5f43e0e0f38331c004b8aa002a3f5f7
sidsan76 <sidsan76@gmail.com>
2020-04-29 02:05:25 +0530

    added console client to use library functionality

---
+++ Spellout.Client/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Spellout.Library;
+
+namespace Spellout.Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            do
+            {
+                Console.WriteLine("Please enter a number (max 19 characters: )");
+
+                string inputNumber = Console.ReadLine ();
+                INumberToWord converter = new IndianSystem();
+                bool isValid = converter.Validate(inputNumber);
+
+                if (isValid)
+                {
+                    Console.WriteLine("Spelling : " + converter.Convert(inputNumber));
+                    Console.ReadLine ();
+                }
+                else 
+                {
+                    Console.WriteLine("Please enter a valid number.");
+                    Console.ReadLine ();
+                }                
+            } while (ShouldContinue());
+        }
+
+        private static bool ShouldContinue()
+        {
+            Console.WriteLine("Do you want to continue?(y/n) :");
+            string response = Console.ReadLine ();
+            return response.ToLower().Trim() == "y";
+        }
+    }
+}
